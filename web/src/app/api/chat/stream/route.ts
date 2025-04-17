aeca1eeacce191a74e506e4cabc4b8782792f6af
Li Xin <lixin.henry@bytedance.com>
2025-04-17 16:43:13 +0800

    chore: add copyright

--- web/src/app/api/chat/stream/route.ts
+++ web/src/app/api/chat/stream/route.ts
@@ -1,3 +1,6 @@
+// Copyright (c) 2025 Bytedance Ltd. and/or its affiliates
+// SPDX-License-Identifier: MIT
+
 import { NextResponse, type NextRequest } from "next/server";
 
 import { env } from "~/env";
