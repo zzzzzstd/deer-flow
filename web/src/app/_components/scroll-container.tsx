2dc94a1cbf35f270ea3156bb7978085ba0c2a1b6
Li Xin <lixin.henry@bytedance.com>
2025-04-20 11:48:24 +0800

    fix: adjust top

--- web/src/app/_components/scroll-container.tsx
+++ web/src/app/_components/scroll-container.tsx
@@ -26,7 +26,7 @@ export function ScrollContainer({
         <>
           <div
             className={cn(
-              "absolute top-0 right-0 left-0 z-10 h-10 bg-gradient-to-b",
+              "absolute top-[-1px] right-0 left-0 z-10 h-10 bg-gradient-to-b",
               `from-[var(--scroll-shadow-color)] to-transparent`,
             )}
             style={
