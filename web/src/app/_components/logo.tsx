c749aff1ae21e344b86f8b5a6129d9cc6288f8e3
Li Xin <lixin.henry@bytedance.com>
2025-04-20 22:30:00 +0800

    feat: change font size

--- web/src/app/_components/logo.tsx
+++ web/src/app/_components/logo.tsx
@@ -9,7 +9,7 @@ export function Logo() {
   const [text, setText] = useState("🦌 DeerFlow");
   return (
     <a
-      className="text-sm opacity-70 transition-opacity duration-300 hover:opacity-100"
+      className="opacity-70 transition-opacity duration-300 hover:opacity-100"
       target="_blank"
       href="https://github.com/bytedance/deer-flow"
       onMouseEnter={() =>
