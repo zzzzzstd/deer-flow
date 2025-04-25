c3689723d5e0ba1e8ce76a4d7b8aa9fd8c4de510
Li Xin <lixin.henry@bytedance.com>
2025-04-25 10:46:58 +0800

    chore: remove copyright

--- web/src/components/ui/button.tsx
+++ web/src/components/ui/button.tsx
@@ -1,6 +1,3 @@
-// Copyright (c) 2025 Bytedance Ltd. and/or its affiliates
-// SPDX-License-Identifier: MIT
-
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import * as React from "react";
